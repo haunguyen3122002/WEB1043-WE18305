f1e90ff5e6c5c4cbad8fce2f30537b5305a7bd3d
haunguyen3122002 <haunvtpd08242@fpt.edu.vn>
2023-03-08 13:39:08 +0700

    nop bai

---
+++ lab1/app.js
@@ -0,0 +1,10 @@
+let a = prompt("moi nhap mot so nguyen a: ");
+//let a1 = parseInt(a);
+
+let b = prompt("moi nhap so nguyen b: ");
+//let b1 = parseInt(b);
+
+let c = parseInt(a) + parseInt(b);
+//let x = a + b;
+document.write(c);
+console.log(c);

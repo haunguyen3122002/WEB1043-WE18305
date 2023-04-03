bded52aaa4fc3c69e1b9bd07a2add0cc3af37785
haunguyen3122002 <haunvtpd08242@fpt.edu.vn>
2023-04-03 11:14:36 +0700

    nop bai

---
+++ lab5/lab5.js
@@ -0,0 +1,24 @@
+function validateInput() {
+  let formElement = document.querySelector(".form")
+  let validateInput = formElement.querySelector(".form-input")
+  for (let i = 0; i < inputElement.legth; i++) {
+    if (inputElement[i].value === "") {
+      inputElement[i].parentElement.querySelector(".error-message").innerText = 'Please enter your ${inputElemetn[i].id}'
+    } else{
+      inputElement[i].parentElement.querySelector(".error-message").innerText = ""
+    }
+}
+}
+function addNew(){
+  validateInput()
+  let formElement = document.querySelector(".form")
+  let errorElement = formElement.querySelectorAll(".error-message")
+  let arrErrorElement = []
+    for (let i=0; i < errorElement.length; i++){
+      arrErrorElement.push(errorElement[i].innerText)
+    }
+    let checkErrorElement = arrErrorElement.every(value => value === "")
+    if (checkErrorElement){
+      
+    }
+}

3fcef21c8d8f638bfdb6b4af35a6dbe7b4da6726
haunguyen3122002 <haunvtpd08242@fpt.edu.vn>
2023-04-04 14:24:09 +0700

    nop bai

--- lab5/lab5.js
+++ lab5/lab5.js
@@ -2,23 +2,52 @@ function validateInput() {
   let formElement = document.querySelector(".form")
   let validateInput = formElement.querySelector(".form-input")
   for (let i = 0; i < inputElement.legth; i++) {
-    if (inputElement[i].value === "") {
+    if (inputElement[i].value === " ") {
       inputElement[i].parentElement.querySelector(".error-message").innerText = 'Please enter your ${inputElemetn[i].id}'
-    } else{
-      inputElement[i].parentElement.querySelector(".error-message").innerText = ""
+    } else {
+      inputElement[i].parentElement.querySelector(".error-message").innerText = " "
     }
+  }
 }
-}
-function addNew(){
+function addNew() {
   validateInput()
   let formElement = document.querySelector(".form")
   let errorElement = formElement.querySelectorAll(".error-message")
   let arrErrorElement = []
-    for (let i=0; i < errorElement.length; i++){
-      arrErrorElement.push(errorElement[i].innerText)
-    }
-    let checkErrorElement = arrErrorElement.every(value => value === "")
-    if (checkErrorElement){
-      
-    }
+  for (let i = 0; i < errorElement.length; i++) {
+    arrErrorElement.push(errorElement[i].innerText)
+  }
+  let checkErrorElement = arrErrorElement.every(value => value === "")
+  if (checkErrorElement) {
+    let name = document.getElementById("name").value
+    let address = document.getElementById("address").value
+    let listStudnt = localStorage.getItem("listStudnt") ? JSON.parse(localStorage.setItem("list-student")) : []
+    listStudnt.push({
+      name: name,
+      address: address
+    })
+    localStorage.setItem("list-student", JSON.stringify(listStudnt))
+  }
+}
+function renderStudent(){
+  let listStudnt = localStorage.getItem("listStudnt") ? JSON.parse(localStorage.setItem("list-student")) : []
+  let student =
+  <tr>
+  <th>ID</th>
+  <th>ID</th>
+  <th>ID</th>
+  <th>ID</th>
+</tr>
+listStudnt.map((value, index)=>{
+  student += <tr>
+  <td>${index + 1}</td>
+  <td>${value.name}</td>
+  <td>${value.address}</td>
+  <td>
+    <buttom>Edit</buttom>
+    <buttom>Delete</buttom>
+  </td>
+  </tr>
+})
+document.getElementById("tableContent").innerHTML = student
 }

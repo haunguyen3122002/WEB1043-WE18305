1f3e581b469da4c55a29f39fb7b7c4c8571e8191
haunguyen3122002 <haunvtpd08242@fpt.edu.vn>
2023-04-07 12:20:30 +0700

    nop bai

--- Assignmet/app.js
+++ Assignmet/app.js
@@ -14,52 +14,83 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
 
 //thongtin
 const form = document.querySelector('form');
 const nameInput = document.querySelector('#name');
 
 form.addEventListener('submit', (event) => {
-    if (nameInput.value === '') {
-        event.preventDefault();
-        alert('Vui lòng nhập họ và tên');
-    }
+  if (nameInput.value === '') {
+    event.preventDefault();
+    alert('Vui lòng nhập họ và tên');
+  }
+});
 
-const form = document.querySelector('form');
-const phoneInput = document.querySelector('#phone');
+  const phoneInput = document.querySelector('#phone');
 
-form.addEventListener('submit', (event) => {
+  form.addEventListener('submit', (event) => {
     const phoneRegex = /^0\d{9}$/;
     if (!phoneRegex.test(phoneInput.value)) {
-        event.preventDefault();
-        alert('Số điện thoại không hợp lệ');
+      event.preventDefault();
+      alert('Số điện thoại không hợp lệ');
     }
-    const form = document.querySelector('form');
-const emailInput = document.querySelector('#email');
+    });
+    const emailInput = document.querySelector('#email');
 
-form.addEventListener('submit', (event) => {
-    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (!emailRegex.test(emailInput.value)) {
+    form.addEventListener('submit', (event) => {
+      const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      if (!emailRegex.test(emailInput.value)) {
         event.preventDefault();
         alert('Email không hợp lệ');
-    }
-});
+      }
+    });
 
-form.addEventListener('submit', (event) => {
-    if (phoneInput.value !== '' && emailInput.value === '') {
+    form.addEventListener('submit', (event) => {
+      if (phoneInput.value !== '' && emailInput.value === '') {
         event.preventDefault();
-    }
-});
-});
-});
+      }
+    });
+
+
+
+
+//hiệu ứng onmouseover
+function bigImg(x) {
+  x.style.height = "250px";
+  x.style.width = "400px";
+}
+
+function normalImg(x) {
+  x.style.height = "100%";
+  x.style.width = "100%";
+}
+
+//vi tri
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+//Media

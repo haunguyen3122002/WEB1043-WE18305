8af70c058417e07c235d6c2ecadee119f4624c12
haunguyen3122002 <haunvtpd08242@fpt.edu.vn>
2023-04-17 12:21:20 +0700

    nop bai

--- Assignmet/app.js
+++ Assignmet/app.js
@@ -1,9 +1,9 @@
 //banner
 var counter = 1;
-setInterval(function(){
+setInterval(function () {
   document.getElementById('radio' + counter).checked = true;
-  counter ++;
-  if(counter > 4){
+  counter++;
+  if (counter > 4) {
     counter = 1;
   }
 }, 5000);
@@ -42,9 +42,9 @@ function showSlides(n) {
 const form = document.querySelector('form');
 const nameInput = document.querySelector('#name');
 
-form.addEventListener('submit', (event) => {
-  if (nameInput.value === '') {
-    event.preventDefault();
+form.addEventListener('submit', (event) => {//hàm addEventListener thêm sự kiện submit vào form
+  if (nameInput.value === '') {//kiểm tra nameinput có rỗng hay k
+    event.preventDefault();//nếu rỗng thì sử dụng hàm preventDefault để ngăn chặn việc submit
     alert('Vui lòng nhập họ và tên');
   }
 });
@@ -57,24 +57,23 @@ form.addEventListener('submit', (event) => {
       event.preventDefault();
       alert('Số điện thoại không hợp lệ');
     }
-    });
-    const emailInput = document.querySelector('#email');
+  });
+  const emailInput = document.querySelector('#email');
 
-    form.addEventListener('submit', (event) => {
-      const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-      if (!emailRegex.test(emailInput.value)) {
-        event.preventDefault();
-        alert('Email không hợp lệ');
-      }
-    });
+  form.addEventListener('submit', (event) => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!emailRegex.test(emailInput.value)) {
+      event.preventDefault();
+      alert('Email không hợp lệ');
+    }
+  });
 
-    form.addEventListener('submit', (event) => {
-      if (phoneInput.value !== '' && emailInput.value === '') {
-        event.preventDefault();
-      }
-    });
-
-
+  form.addEventListener('submit', (event) => {
+    if (phoneInput.value !== '' && emailInput.value === '') {
+      event.preventDefault();
+      alert('Succesfull contact');
+    }
+  });
 
 
 //hiệu ứng onmouseover
@@ -88,20 +87,3 @@ function normalImg(x) {
   x.style.width = "100%";
 }
 
-//vi tri
-var x = document.getElementById("demo");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
-
-//Media
